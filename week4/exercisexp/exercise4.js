9aac9da930a21e9ec8c060c820a7d3eb7f03e09b
ahmed-zai <saidahmed0703@gmail.com>
2025-07-09 21:31:44 +0100

    exercise4

---
+++ week4/exercisexp/exercise4.js
@@ -0,0 +1,10 @@
+/*
+🌟 Exercise 4 : Find the sum
+Instructions
+
+    Create a one line function (ie. an arrow function) that receives two numbers as parameters and returns the sum.
+
+
+*/
+const sum = (a ,b) => a + b;
+console.log(sum(5, 10)); // Logs: 15
