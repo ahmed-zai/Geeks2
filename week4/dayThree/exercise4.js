b25e2a2e0c211e8c3b9d035c38ea7882e29e631c
ahmed-zai <saidahmed0703@gmail.com>
2025-07-15 11:11:34 +0100

    exercise4

---
+++ week4/dayThree/exercise4.js
@@ -0,0 +1,20 @@
+/*
+Exercise 4 : Person class
+Instructions
+
+    Analyze the code below. What will be the output?
+
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+const member = new Person('John');
+console.log(typeof member);
+
+*/
+// the output will be:
+// object 
+// explanation: the code defines a class Person and creates an isnstance of it, which is an object 
+// and the typeof operator returns 'object' for instance of classes in js 
